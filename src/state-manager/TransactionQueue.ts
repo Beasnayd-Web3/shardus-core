e5bdec3e9e3a69bb6fc5df704d8fdca12d2d9df0
Thant Sin Toe <mr.thantsintoe@gmail.com>
2024-05-10 23:21:32 +0700

    added missing continue statement in "await final data" state

--- src/state-manager/TransactionQueue.ts
+++ src/state-manager/TransactionQueue.ts
@@ -5919,6 +5919,7 @@ class TransactionQueue {
                 if (logFlags.debug) this.mainLogger.debug(`shrd_awaitFinalData_challenge : ${queueEntry.logID} challenge from receipt2`)
                 this.updateTxState(queueEntry, 'fail')
                 this.removeFromQueue(queueEntry, currentIndex)
+                continue
               } if (receipt2 == null && queueEntry.receivedBestChallenge) {
                 const enoughUniqueChallenges = queueEntry.uniqueChallengesCount >= configContext.stateManager.minRequiredChallenges
                 if (enoughUniqueChallenges) {
