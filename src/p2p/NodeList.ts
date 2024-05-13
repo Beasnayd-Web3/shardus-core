7e01980d5444350f90cc2d3f4a376a44f088b5e9
Sam Sweet <sam.w.sweet@gmail.com>
2024-05-13 13:21:06 -0400

    remove initializing state

--- src/p2p/NodeList.ts
+++ src/p2p/NodeList.ts
@@ -31,7 +31,6 @@ export let activeByIdOrder: P2P.NodeListTypes.Node[]
 export let syncingByIdOrder: P2P.NodeListTypes.Node[]
 export let selectedByIdOrder: P2P.NodeListTypes.Node[]
 export let standbyByIdOrder: P2P.NodeListTypes.Node[]
-export let initializingByIdOrder: P2P.NodeListTypes.Node[]
 export let readyByTimeAndIdOrder: P2P.NodeListTypes.Node[]
 export let activeOthersByIdOrder: P2P.NodeListTypes.Node[]
 export let potentiallyRemoved: Set<P2P.NodeListTypes.Node['id']>
@@ -83,7 +82,6 @@ export function reset(caller: string) {
   syncingByIdOrder = []
   selectedByIdOrder = []
   standbyByIdOrder = []
-  initializingByIdOrder = []
   readyByTimeAndIdOrder = []
   activeOthersByIdOrder = []
   potentiallyRemoved = new Set()
@@ -122,10 +120,6 @@ export function addNode(node: P2P.NodeListTypes.Node, caller: string) {
     insertSorted(othersByIdOrder, node, propComparator('id'))
   }
 
-  // If initializing, insert into initializingByIdOrder
-  if (node.status === P2P.P2PTypes.NodeStatus.INITIALIZING) {
-    insertSorted(initializingByIdOrder, node, propComparator('id'))
-  }
   // If selected, insert into standbyByIdOrder
   if (node.status === P2P.P2PTypes.NodeStatus.STANDBY) {
     insertSorted(standbyByIdOrder, node, propComparator('id'))
@@ -203,9 +197,6 @@ export function removeNode(
   }
 
   // Remove from arrays
-  idx = binarySearch(initializingByIdOrder, { id }, propComparator('id'))
-  if (idx >= 0) initializingByIdOrder.splice(idx, 1)
-
   idx = binarySearch(selectedByIdOrder, { id }, propComparator('id'))
   if (idx >= 0) selectedByIdOrder.splice(idx, 1)
 
