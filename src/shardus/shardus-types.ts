a61870e284991a3fba7e526793d54b5b475188d3
Abdul Azeem <sabdulazeem01@gmail.com>
2024-07-09 12:39:05 +0300

    chore: Add debug flag for NTP bogus offset decrements

--- src/shardus/shardus-types.ts
+++ src/shardus/shardus-types.ts
@@ -1085,6 +1085,8 @@ export interface ServerConfiguration {
     randomCycleData: boolean
     // Maximum number of elements allowed in txDebugStatList map
     debugStatListMaxSize: number
+    // flag to add bogus increments to NTP offset
+    debugNTPBogusDecrements: boolean
   }
   /** Options for the statistics module */
   statistics?: {
